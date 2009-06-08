e22b7301bcc4558acc8552b5ea6f167be15ecdde
matt <matt@obsidian.sml>
2009-06-08 15:59:59 +1200

    Tool for matthew, to make and output all files.

---
+++ doall.sh
@@ -0,0 +1,4 @@
+make
+./parse-bacs nintendochan.cs > /www/tracescript/blog/nintendochan.cs.new.txt
+./parse-bacs banner.cs > /www/tracescript/blog/banner.cs.new.txt
+./parse-bacs icon.cs > /www/tracescript/blog/icon.cs.new.txt
